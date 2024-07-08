85fd8a4f3fc5c58804d751e81236c8de402c49b9
Sania313 <91427982+Sania313@users.noreply.github.com>
2024-07-08 22:13:43 +0500

    SQL_Practice.sql

--- SQL_Prac.sql
+++ SQL_Prac.sql
@@ -1,4 +1,14 @@
+Q#1:
 /* Query all columns for all American cities in the CITY table with populations larger than 100000.
  The CountryCode for America is USA. 
 */
+
 select* from CITY as c where c.POPULATION >100000 and c.COUNTRYCODE = 'USA';
+
+Q#2: 
+/*
+Query the NAME field for all American cities in the CITY table with populations larger than 120000.
+The CountryCode for America is USA. */
+
+Select name from city as c where c.countrycode ='USA' and c.population>120000;
+
